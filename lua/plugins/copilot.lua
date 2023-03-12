0ab7415f92448bead5739a16d1a0ea777f0a6276
koda <alexandre.decobert@epitech.eu>
2023-03-12 08:09:36 +0100

    [ADD] settings copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,5 @@
+-- Copilot configuration
+vim.g.copilot_display_context = 1 -- afficher le contexte de la suggestion
+vim.g.copilot_max_lines = 5 -- nombre de lignes affichées pour les suggestions
+vim.g.copilot_selected_bg = "#5e4ae3" -- couleur de fond de la suggestion sélectionnée
+vim.g.copilot_selected_border = "none" -- supprimer la bordure de la suggestion sélectionnée
